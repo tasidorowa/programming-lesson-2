e11d65af5710b9c553a3b0d1fe788f5828a0432d
Tatyana Sidorova <tasidorowa@gmail.com>
2024-02-23 23:55:38 +0300

    Добавили задание 2

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿int N = 10;
+int[] arr = new int[N];
+int i = 0;
+
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    //Console.Write(arr[i] + " ");
+    Console.Write($"{arr[i]} ");
+    i++;
+}
