e087593a80c881bce4d7e43a4185e35f8e99dd60
Tatyana Sidorova <tasidorowa@gmail.com>
2024-02-24 00:04:30 +0300

    Добавили задание 3

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int[] array = [3, 5, 12, 78, 45, 23, 5, 9, 11, 56];
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0) Console.Write($"{array[i]} "); // control + c - остановить выполнение
+    i++;
+}
